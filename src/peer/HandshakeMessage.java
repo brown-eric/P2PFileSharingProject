e7305538067805048f802387d2b595634095d3ef
Eric Brown <128325212+brown-eric@users.noreply.github.com>
2025-10-19 19:41:23 -0400

    Placeholder
    Added methods for encoding and decoding handshake messages.

--- src/peer/HandshakeMessage.java
+++ src/peer/HandshakeMessage.java
@@ -1,1 +1,19 @@
+package peer;
+
+public class HandshakeMessage {
+    // Constants for header and message length
+    public static final String HEADER = "P2PFILESHARINGPROJ";
+    public static final int HANDSHAKE_LENGTH = 32;
+
+    // Placeholder methods for encoding/decoding handshake
+    public byte[] getBytes(int peerId) {
+        // TODO: Build handshake byte array
+        return new byte[HANDSHAKE_LENGTH];
+    }
+
+    public static HandshakeMessage fromBytes(byte[] data) {
+        // TODO: Parse handshake from bytes
+        return new HandshakeMessage();
+    }
+}
 
