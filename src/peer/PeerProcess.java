188f06fde52089c758c70cb851fd92f859cf9bbf
Eric Brown <128325212+brown-eric@users.noreply.github.com>
2025-10-19 19:40:59 -0400

    Placeholder
    Added main method and startup message for PeerProcess.

--- src/peer/PeerProcess.java
+++ src/peer/PeerProcess.java
@@ -1,1 +1,9 @@
+package peer;
+
+public class PeerProcess {
+    public static void main(String[] args) {
+        // Entry point for the peer process
+        System.out.println("PeerProcess started.");
+    }
+}
 
