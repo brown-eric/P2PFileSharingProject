1b2326cf17480b04e9dd33203d748306d23b9f3d
Eric Brown <128325212+brown-eric@users.noreply.github.com>
2025-10-19 19:42:21 -0400

    Placeholder
    Added a Logger class with a log method for logging messages.

--- src/utils/Logger.java
+++ src/utils/Logger.java
@@ -1,1 +1,9 @@
+package utils;
+
+public class Logger {
+    public static void log(String message) {
+        // TODO: Print and write log messages to a file
+        System.out.println(message);
+    }
+}
 
